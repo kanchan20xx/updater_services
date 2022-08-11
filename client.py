ab2d3c327e3b27be67177e3005aa69fc49e2ffbb
kanchan20xx <kanchan20xx@gmail.com>
2022-08-11 17:56:37 +0900

    Implement cilent side for testing.

--- client.py
+++ client.py
@@ -4,13 +4,15 @@ from proto import service_pb2
 from proto import service_pb2_grpc
 from proto import env_repo_service_pb2
 from proto import env_repo_service_pb2_grpc
+from proto import updater_recipe_service_pb2
+from proto import updater_recipe_service_pb2_grpc
 
 def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = service_pb2_grpc.SampleServiceStub(channel)
-        response = stub.SayHello(service_pb2.HelloRequest(name='Yamada'))
-    print('RECV : %s' % response.message)
-    print("%d" % len(response.ids))
+    # with grpc.insecure_channel('localhost:50051') as channel:
+        # stub = service_pb2_grpc.SampleServiceStub(channel)
+        # response = stub.SayHello(service_pb2.HelloRequest(name='Yamada'))
+    # print('RECV : %s' % response.message)
+    # print("%d" % len(response.ids))
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = env_repo_service_pb2_grpc.EnvRepoServiceStub(channel=channel)
         #response = stub.GetEnvInfo(env_repo_service_pb2.GetEnvInfoReply())
@@ -18,6 +20,11 @@ def run():
         print(len(response.diff_files))
         response = stub.GetTargetableTags(env_repo_service_pb2.Empty())
         print(response.tags[0].tag_name)
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = updater_recipe_service_pb2_grpc.UpdaterRecipeServiceStub(channel=channel)
+        response = stub.GetPackages(updater_recipe_service_pb2.Empty())
+        for responsed_pkg in response.packages:
+            print(responsed_pkg.package_name)
 
 if __name__ == '__main__':
     run()
